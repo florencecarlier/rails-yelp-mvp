28752ce30596eaa107e1a3066081358e810185ea
Florence Carlier <florence.carlier.5@gmail.com>
2020-05-13 17:10:14 +0200

    restaurants et models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+restaurant_attributes = [
+  {
+    name: "Miam Miam",
+    address: "Namur",
+    category: "belgian",
+  },
+  {
+    name:"Corona Virus",
+    address:"Wuhan",
+    category:"chinese",
+  },
+  {
+    name:"Firenze",
+    address:"Florence",
+    category:"italian",
+  },
+  {
+    name:"Le Bon Melanchon",
+    address:"Paris",
+    category:"french",
+  },
+  {
+    name:"Nagazaki",
+    address:"Tokyo",
+   category:"japanese",
+  }
+]
+
+restaurant_attributes.each { |params| Restaurant.create!(params)}
+
+
